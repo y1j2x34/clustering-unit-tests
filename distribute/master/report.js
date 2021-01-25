256b1588bb2a2713c1419ac137f5ec469f03509d
y1j2x34 <y1j2x34@qq.com>
2021-01-25 23:33:36 +0800

    feat: coverage reporter

--- distribute/master/report.js
+++ distribute/master/report.js
@@ -1,19 +1,39 @@
+const path = require('path');
+const fs = require('fs');
 const istanbul = require('istanbul-lib-coverage');
 const { mergeKarmaResults } = require('../../scripts/merge-karma-json')
 
 exports.report = function report(coverages, karmaResults) {
-    const map = istanbul.createCoverageMap();
-    coverages.forEach((it) => {
-        map.merge(it);
-    });
-    const mergedCoverageJSON = map.toJSON();
-
     const mergedKarmaResut = mergeKarmaResults(karmaResults);
+    
+    reportCoverage(coverages);
 
-    const coverageSumary = map.getCoverageSummary();
+}
 
-    console.log(mergedCoverageJSON);
-    console.log(coverageSumary);
-    console.log(mergedKarmaResut);
-    // convert html
+function reportCoverage(coverages) {
+    const coverageMap = istanbul.createCoverageMap();
+    coverages.forEach((it) => {
+        coverageMap.merge(it);
+    });
+    const libReport = require('istanbul-lib-report');
+    const reports = require('istanbul-reports');
+    const libSourcemap = require('istanbul-lib-source-maps');
+    const sourcemapStore = libSourcemap.createSourceMapStore({
+        baseDir: path.resolve('../../lib')
+    });
+    sourcemapStore.transformCoverage(coverageMap).then(remappedCoverageMap => {
+        
+        const context = libReport.createContext({
+            dir: path.resolve(__dirname, '../../final-report/coverage'),
+            coverageMap: remappedCoverageMap
+        });
+    
+        const tree = context.getTree('pkg')
+    
+        const report = reports.create('html', {
+            skipEmpty: false,
+            skipFull: false
+        });
+        tree.visit(report, context);
+    });
 }
---
+++ final-report/README.md
@@ -0,0 +1,3 @@
+# reports
+
+test:master 将集群的各个节点数据汇总后生成测试报告。
